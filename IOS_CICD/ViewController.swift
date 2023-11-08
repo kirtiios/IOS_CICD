0d70e1d2158ef6225de7a3fe3ec9850928fabc0a
kirti chavda <kirti@amnex.com>
2023-11-08 15:28:08 +0530

    [fastlane] Updated development and platform ios

---
+++ IOS_CICD/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  IOS_CICD
+//
+//  Created by Sandip Patel on 08/11/23.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        FirebaseApp.configure()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
